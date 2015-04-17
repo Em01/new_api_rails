ac1800dc1983605becff88473b7d3c7b910c9814
Em01 <emma_williams1@hotmail.co.uk>
2015-04-17 23:51:44 +0100

    basic setting of api

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ NewMarketPlaceApi::Application.routes.draw do
  # Api definition
   namespace :api, defaults: { format: :json } 
                               constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
     # We are going to list our resources here
+    end
   end
 end
 
