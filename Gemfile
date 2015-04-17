2efe908ed98ca6d89291a73e50c7c3a61ef714d2
Em01 <emma_williams1@hotmail.co.uk>
2015-04-18 00:00:30 +0100

    Adds devise user model

--- Gemfile
+++ Gemfile
@@ -29,3 +29,5 @@ group :test do
   gem 'factory_girl_rails'
   gem 'ffaker'
 end
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'api_constraints'
+
 NewMarketPlaceApi::Application.routes.draw do
+  devise_for :users
  # Api definition
-  namespace :api, defaults: { format: :json } 
+  namespace :api, defaults: { format: :json }, 
                               constraints: { subdomain: 'api' }, path: '/'  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
@@ -8,4 +11,8 @@ NewMarketPlaceApi::Application.routes.draw do
     end
   end
 end
+
+
+
+
 
