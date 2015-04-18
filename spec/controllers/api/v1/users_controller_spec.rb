376eec0df2649adfa5ef4c9440b20d5d5027f3f2
Em01 <emma_williams1@hotmail.co.uk>
2015-04-18 13:39:47 +0100

    amended headers in controller spec users

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
 
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.newmarketplace.v1" }
+  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1, #{Mime::JSON}" }
 
+  before(:each) { request.headers['Content-Type'] = Mime::JSON.to_s }
+  
   describe "GET #show" do
     before(:each) do 
       @user = FactoryGirl.create :user
